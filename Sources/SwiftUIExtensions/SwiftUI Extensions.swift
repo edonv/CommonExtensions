4802e2f40c1f35b729a31742d18229c3623b3511
edonv <22782929+edonv@users.noreply.github.com>
2022-08-17 18:38:36 -0400

    Update SwiftUI Extensions.swift
    Added `public` to `??` operator.

--- Sources/SwiftUIExtensions/SwiftUI Extensions.swift
+++ Sources/SwiftUIExtensions/SwiftUI Extensions.swift
@@ -12,7 +12,7 @@ import SwiftUI
 
 @available(OSX 10.15, iOS 13, *)
 extension Binding {
-    static func ?? (lhs: Binding<Optional<Value>>, rhs: Value) -> Binding<Value> {
+    public static func ?? (lhs: Binding<Optional<Value>>, rhs: Value) -> Binding<Value> {
         Binding(
             get: { lhs.wrappedValue ?? rhs },
             set: { lhs.wrappedValue = $0 }
