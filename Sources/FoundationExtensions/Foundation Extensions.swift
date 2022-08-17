0f075a2dcbb21ea7f1466e735bfa2c961cd95512
edonv <22782929+edonv@users.noreply.github.com>
2022-08-17 18:37:06 -0400

    Update Foundation Extensions.swift
    Added `public` to `KeyPath`-based `sorted` overload.

--- Sources/FoundationExtensions/Foundation Extensions.swift
+++ Sources/FoundationExtensions/Foundation Extensions.swift
@@ -10,8 +10,8 @@ import Foundation
 // MARK: - Array Sorting by KeyPath
 
 extension Array {
-    func sorted<T>(by keyPath: KeyPath<Element, T>,
-                   _ areInIncreasingOrder: (T, T) throws -> Bool) rethrows -> Self {
+    public func sorted<T>(by keyPath: KeyPath<Element, T>,
+                          _ areInIncreasingOrder: (T, T) throws -> Bool) rethrows -> Self {
         return try self.sorted(by: { try areInIncreasingOrder($0[keyPath: keyPath], $1[keyPath: keyPath]) })
     }
 }
