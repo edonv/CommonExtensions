51da35856f7c278ecf0a82e5cae5d83026670ee1
edonv <22782929+edonv@users.noreply.github.com>
2022-08-17 18:06:32 -0400

    Update SwiftUI Extensions.swift
    Added Added in SwiftUI extensions

--- Sources/SwiftUIExtensions/SwiftUI Extensions.swift
+++ Sources/SwiftUIExtensions/SwiftUI Extensions.swift
@@ -5,3 +5,16 @@
 //  Created by Edon Valdman on 8/17/22.
 //
 
+import Foundation
+import SwiftUI
+
+// MARK: - Optional Binding Operator
+
+extension Binding {
+    static func ?? (lhs: Binding<Optional<Value>>, rhs: Value) -> Binding<Value> {
+        Binding(
+            get: { lhs.wrappedValue ?? rhs },
+            set: { lhs.wrappedValue = $0 }
+        )
+    }
+}
