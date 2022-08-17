4e273b924813809ce3777a41f836a90b11b794dd
edonv <22782929+edonv@users.noreply.github.com>
2022-08-17 18:06:20 -0400

    Update Foundation Extensions.swift
    Added in Foundation extensions

--- Sources/FoundationExtensions/Foundation Extensions.swift
+++ Sources/FoundationExtensions/Foundation Extensions.swift
@@ -5,3 +5,13 @@
 //  Created by Edon Valdman on 8/17/22.
 //
 
+import Foundation
+
+// MARK: - Array Sorting by KeyPath
+
+extension Array {
+    func sorted<T>(by keyPath: KeyPath<Element, T>,
+                   _ areInIncreasingOrder: (T, T) throws -> Bool) rethrows -> Self {
+        return try self.sorted(by: { try areInIncreasingOrder($0[keyPath: keyPath], $1[keyPath: keyPath]) })
+    }
+}
