8ebce1764a63351976bfc40b6c556d97a2e8ecbc
edonv <22782929+edonv@users.noreply.github.com>
2022-09-09 14:06:23 -0400

    Update Misc Combine Extensions.swift
    Added an overload of `Publisher.filter` that compares the "`KeyPath`-ed" property of all incoming elements to the provided `aValue`.

--- Sources/CombineExtensions/Misc Combine Extensions.swift
+++ Sources/CombineExtensions/Misc Combine Extensions.swift
@@ -97,3 +97,15 @@ extension Publisher where Failure: Error, Failure: Equatable {
         self.replaceError(with: output) { $0 == error }
     }
 }
+
+@available(OSX 10.15, iOS 13, *)
+extension Publisher {
+    /// Republishes all elements whose provided property match a provided value.
+    /// - Parameters:
+    ///   - keyPath: `KeyPath` to a property whose value should be compared.
+    ///   - aValue: Value to be compared to provided property of each incoming element.
+    /// - Returns: A publisher that republishes all elements whos provided property match `aValue`.
+    public func filter<T: Equatable>(_ keyPath: KeyPath<Output, T>, equals aValue: T) -> Publishers.Filter<Self> {
+        self.filter { $0[keyPath: keyPath] == aValue }
+    }
+}
