58bd4975b24db056806db43a4c7a8a7bd9f40157
edonv <22782929+edonv@users.noreply.github.com>
2022-09-17 12:35:17 -0400

    Update Misc Combine Extensions.swift
    Added new Flat Map Zip publishers

--- Sources/CombineExtensions/Misc Combine Extensions.swift
+++ Sources/CombineExtensions/Misc Combine Extensions.swift
@@ -108,3 +108,47 @@ extension Publisher {
         self.filter { $0[keyPath: keyPath] == aValue }
     }
 }
+
+// MARK: - Flat Map Zip
+
+@available(OSX 10.15, iOS 13, *)
+extension Publisher {
+    /// Combines elements from another publisher, created by transforming elements of the upstream publisher,
+    /// and deliver pairs of elements as tuples.
+    /// - Parameters:
+    ///   - maxPublishers: Specifies the maximum number of concurrent publisher subscriptions, or unlimited
+    ///   if unspecified.
+    ///   - transform: A closure that takes elements of the upstream publisher as a parameter and returns a
+    ///   transformed publisher.
+    /// - Returns: A publisher that publishes zipped elements from the upstream publisher with elements of the
+    /// transformed publisher.
+    public func flatMapZip<P>(maxPublishers: Subscribers.Demand = .unlimited, _ transform: @escaping (Self.Output) throws -> P) -> AnyPublisher<(Self.Output, P.Output), Self.Failure> where P: Publisher, Self.Failure == P.Failure {
+        self
+            .tryFlatMap(maxPublishers: maxPublishers) { result in
+                return Publishers.Zip(Just(result)
+                                        .setFailureType(to: Self.Failure.self)
+                                        .eraseToAnyPublisher(),
+                                      try transform(result))
+            }.eraseToAnyPublisher()
+    }
+    
+    /// Combines elements from another publisher, created by transforming elements of the upstream publisher,
+    /// and deliver pairs of elements as tuples.
+    /// - Parameters:
+    ///   - maxPublishers: Specifies the maximum number of concurrent publisher subscriptions, or unlimited
+    ///   if unspecified.
+    ///   - keyPath: The key path of a property on `Output`.
+    ///   - transform: A closure that takes a property of elements of the upstream publisher as a parameter and
+    ///   returns a transformed publisher.
+    /// - Returns: A publisher that publishes zipped elements from the upstream publisher with elements of the
+    /// transformed publisher.
+    public func flatMapZip<T, P>(maxPublishers: Subscribers.Demand = .unlimited, _ keyPath: KeyPath<Self.Output, T>, _ transform: @escaping (T) throws -> P) -> AnyPublisher<(Self.Output, P.Output), Self.Failure> where P: Publisher, Self.Failure == P.Failure {
+        self
+            .tryFlatMap(maxPublishers: maxPublishers) { result in
+                return Publishers.Zip(Just(result)
+                                        .setFailureType(to: Self.Failure.self)
+                                        .eraseToAnyPublisher(),
+                                      try transform(result[keyPath: keyPath]))
+            }.eraseToAnyPublisher()
+    }
+}
