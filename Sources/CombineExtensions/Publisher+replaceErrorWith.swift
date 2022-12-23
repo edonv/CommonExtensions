1f2204b817a691d11458f7423225be130a133354
Edon <edon@valdman.works>
2022-12-23 12:51:48 -0500

    Update Publisher+replaceErrorWith.swift
    Changed order of parameters of `replaceError(_:with:)` to more closely match `replaceError(with:_)` (with handler).

--- Sources/CombineExtensions/Publisher+replaceErrorWith.swift
+++ Sources/CombineExtensions/Publisher+replaceErrorWith.swift
@@ -39,9 +39,9 @@ extension Publisher where Failure: Error, Failure: Equatable {
     /// Handles errors from an upstream publisher by replacing it with another publisher, but only if the
     /// error matches the provided error.
     /// - Parameters:
-    ///   - error: `Error` to replace.
     ///   - output: Value to replace the error with.
-    public func replaceError(_ error: Self.Failure, with output: Output) -> Publishers.Catch<Self, AnyPublisher<Output, Failure>> {
+    ///   - error: `Error` to replace.
+    public func replaceError(with output: Output, _ error: Self.Failure) -> Publishers.Catch<Self, AnyPublisher<Output, Failure>> {
         self.replaceError(with: output) { $0 == error }
     }
 }
