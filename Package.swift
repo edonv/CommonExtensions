d32035487ffcb5d8d1566ba24997967a8037a65d
Edon <edon@valdman.works>
2024-10-01 12:55:11 -0500

    Update Package.swift
    Added exported product called `CommonExtensions` that can be depended on that contains all the targets.

--- Package.swift
+++ Package.swift
@@ -8,6 +8,10 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
+            name: "CommonExtensions",
+            targets: ["CombineExtensions", "SwiftUIExtensions", "FoundationExtensions"]
+        ),
+        .library(
             name: "CombineExtensions",
             targets: ["CombineExtensions"]
         ),
